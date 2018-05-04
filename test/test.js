3ad7458f14f6b6dcacc8db626b8d3270e950f322
Erick <ejdiaz97@hotmail.com>
2018-05-04 02:10:19 -0600

    cambio
    cambio

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var v = new Vista (), usuario = new User();
 
 var arrayTest =[
 	{"id" : "01" , "name" : "erick", "score" : "100"},
-	{"id" : "02" , "name" : "gohn", "score" : "80"},
+	{"id" : "02" , "name" : "john", "score" : "80"},
 	{"id" : "03" , "name" : "marco", "score" : "90"},
 	{"id" : "04" , "name" : "sofia", "score" : "40"},
 	{"id" : "05" , "name" : "maria", "score" : "70"}
